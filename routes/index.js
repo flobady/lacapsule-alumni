ede3e8bd8ab0650380d8d45140cd4f2da12acfa4
Florian Lobady <flobady@yahoo.fr>
2018-02-05 09:52:18 +0100

    ajout de la page profil

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render('profile', );
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', );
+});
+
 module.exports = router;
