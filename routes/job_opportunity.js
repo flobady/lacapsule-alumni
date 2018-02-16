94c64f872e3e96314803e0dbeea5638ff56e0916
athmane2017 <athmane.mef@gmail.com>
2018-02-16 14:00:42 +0100

    correctif pour release front page UI

--- routes/job_opportunity.js
+++ routes/job_opportunity.js
@@ -1,7 +1,7 @@
 // START HOME EXPRESS
 var express = require("express");
 var router = express.Router();
-var requireLogin = require('../middlewares/requireLogin');
+var requireLogin = require("../middlewares/requireLogin");
 
 // Initialisation du module dans le projet
 var mongoose = require("mongoose");
@@ -17,21 +17,36 @@ router.post("/publish", function(req, res, next) {
     postDate: req.body.postDate,
     jobTitle: req.body.jobTitle,
     jobCompany: req.body.jobCompany,
-    jobRegion: req.body.jobRegion,
+    jobCity: req.body.jobCity,
     jobSalary: req.body.jobSalary,
     jobContractType: req.body.jobContractType,
-    jobInfo: req.body.jobInfo
+    jobExperience: req.body.jobExperience,
+    jobResume: req.body.jobResume,
+    jobInfo: req.body.jobInfo,
+    jobContact: req.body.jobContact,
+    jobReference: req.body.jobReference
   });
   // Ecriture des données
   newJob.save(function(error, job) {
     console.log(job);
-    res.render("job_opportunity", { user: req.session.user });
+    // récup sur la base Mongo et affiche sur la même page
+    JobModel.find(function(err, jobList) {
+      console.log(jobList);
+      console.log("La Joblist s'affiche");
+      res.render("job_opportunity", {
+        jobList: jobList,
+        user: req.session.user
+      });
+    });
   });
 });
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("job_opportunity", { user: req.session.user });
+  JobModel.find(function(err, job) {
+    console.log(job);
+    res.render("job_opportunity", { jobList: job, user: req.session.user });
+  });
 });
 
 module.exports = router;
